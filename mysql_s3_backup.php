0e3b2c7cf45025bb55f3b6bdf8391a6afbd8f9d1
Ben Kennish <ben@fubra.com>
2014-06-19 17:08:46 +0100

    handle SIGINT etc. tidied error/notice mechnism. delete backup files on creation failure

--- mysql_s3_backup.php
+++ mysql_s3_backup.php
@@ -2,74 +2,102 @@
 <?php
 
 // Based on vc-backup.pl & cb-backup.pl written by Mark Sutton, December 2011
-// Modified for PHP and extended by Ben Kennish, from November 2012
+// Modified for PHP and extended by Ben Kennish, from November 2012 onwards
 // Awaiting contributions from Nicola Asuni, January 2013, that will sadly never come ;-(
 
-// == TODO List ==
+/*
+== TODO List ==
 
-//FEATURE: differential backup - a diff of the changes between last dump and current dump (to reduce backup sizes)
-//      but this means dependency problem plus lots of disk space
-//FEATURE: if a password is specified in config.inc.php, create a (rwx------) temp config file and use 'mysql --defaults-file'
-//FEATURE: option to use mysqlhotcopy for local MyISAM backups
-//FEATURE: option to use an S3 mount point rather than s3cmd so we can do it all in one piped command - but then what can we do if S3 connection dies?
-//FEATURE: option to produce a report (in XML?) listing backups and the size and time taken for backups and to upload to S3
-//FEATURE: option to enable/disable gpg and to enable/disable the S3 upload (for taking backups to use in another fashion)
+FEATURE: use s3cmd to trim old backups (using s3cmd ls and s3cmd del)
+FEATURE: option to produce a report (in XML?) listing backups and the size and time taken for backups and to upload to S3
+FEATURE: option to use mysqlhotcopy for local MyISAM backups
+FEATURE: option to enable/disable gpg and to enable/disable the S3 upload (for taking backups to use in another fashion)
+FEATURE: option to create a DB dump, upload to S3, and then remove local file on a PER-DATABASE rather than per-server basis
+         + advantage: less disk space used
+         - disadvantage: if you are taking DB down with exec_pre, it'll be down for longer this way
+FEATURE: option to use an S3 mount point rather than s3cmd so we can do it all in one piped command
+           - but then we don't have a local copy if S3 connection dies
+FEATURE: differential backup - a diff of the changes between last dump and current dump (to reduce backup sizes)
+         - but this means dependency problem plus lots of disk space
+*/
 
-//FEATURE: to minimise disk space usage, create dump, upload to S3, and then remove local file on a PER-DATABASE rather than per-server basis
-
-//TIDY: make sure errors go to STDERR and everything else to STDOUT (for cron)
-//TIDY: better logging and output control in general
-//TIDY: don't run exec_post if we haven't run exec_pre?
-//TIDY: cat /dev/null | s3cmd    to stop it reading from STDIN? or does PHP do that anyway?
-
-
-
-function on_error($errno, $errstr, $errfile, $errline, $errcontext)
+// signal handler function
+function sig_handler($signo)
 {
-    // write an entry to the log
-    global $ms3b_cfg;
-
-    $error_types = array( E_WARNING => 'Warning',
-                          E_NOTICE => 'Notice',
-                          E_USER_ERROR => 'User Error',
-                          E_USER_WARNING => 'User Warning',
-                          E_USER_NOTICE => 'User Notice',
-                        );
-    $err_name = (isset($error_types[$errno])) ? $error_types[$errno] : "Error type '$errno'";
-
-    error_log('['.date('Y-m-d H:i:s')."] $err_name - $errstr (line $errline in $errfile)\n", 3, $ms3b_cfg['log']);
-
-    return false; //pass through to PHP's default error handler
+    switch ($signo)
+    {
+        case SIGTERM:
+            // handle shutdown tasks
+            trigger_error('Caught SIGTERM - exiting', E_USER_ERROR);
+            exit;
+            break;
+        case SIGINT:
+            // handle shutdown tasks
+            trigger_error('Caught SIGINT - exiting', E_USER_ERROR);
+            exit;
+            break;
+        case SIGHUP:
+            // handle restart tasks
+            trigger_error('Caught SIGHUP - ignoring', E_USER_WARNING);
+            break;
+        case SIGUSR1:
+            trigger_error('Caught SIGUSR1 - ignoring', E_USER_WARNING);
+            break;
+        default:
+            trigger_error('Caught signal #'.$signo.' - ignoring', E_USER_WARNING);
+            break;
+            // handle all other signals
+    }
 }
 
+// setup signal handlers
+pcntl_signal(SIGTERM, "sig_handler");
+pcntl_signal(SIGINT,  "sig_handler");
+pcntl_signal(SIGHUP,  "sig_handler");
+pcntl_signal(SIGUSR1, "sig_handler");
 
 
 function on_shutdown()
 {
-    global $clean_shutdown;
+    global $clean_shutdown, $server;
     
     if (!$clean_shutdown)
-        echo 'Shutdown was not clean!'.PHP_EOL;
+    {
+        trigger_error('Unclean shutdown!', E_USER_WARNING);
+    }
 
-    global $server;
     if (!empty($server['exec_post']))
     {
-        echo "Running: $server[exec_post]\n";
+        log_notice("Running: $server[exec_post]");
         system($server['exec_post'], $ret);
         if ($ret)
-            fwrite(STDERR, "Warning: exec_post ($server[exec_post]) returned $ret\n");
+            trigger_error("Warning: exec_post ($server[exec_post]) returned $ret", E_USER_WARNING);
     }
+
+    log_notice("on_shutdown() complete. Goodbye!");
 }
 
 
+function log_notice($msg)
+{
+    global $ms3b_cfg;
 
-$clean_shutdown = false;
-register_shutdown_function('on_shutdown');
+    if (!empty($ms3b_cfg['log']))
+        error_log('['.date('d-M-Y H:i:s').'] * '.$msg."\n", 3, $ms3b_cfg['log']);
+
+    echo $msg.PHP_EOL;
+}
 
 
 require_once(dirname(__FILE__).'/config.inc.php');
 
-set_error_handler('on_error');
+$clean_shutdown = false;
+register_shutdown_function('on_shutdown');
+
+ini_set('log_errors', 1);
+ini_set('error_log', $ms3b_cfg['log']);
+ini_set('display_errors', 1);
+
 
 // paranoid mode - give no perms to group/others for any files/dirs we create
 umask(0077);
@@ -90,30 +118,44 @@ else
 if (empty($ms3b_cfg['compressor_cmd']))
     trigger_error('No compressor_cmd set in config', E_USER_ERROR);
 
-error_log('['.date('Y-m-d H:i:s')."] * mysql_s3_backup starting\n", 3, $ms3b_cfg['log']);
+log_notice("mysql_s3_backup starting");
 
 foreach ($ms3b_cfg['Servers'] as $server)
 {
-    echo "Beginning backup (host:'$server[host]', user:'$server[user]')...\n";
+    log_notice("Beginning backup (host:'$server[host]', user:'$server[user]')...");
 
     $now = date('Y-m-d_H.i.s');
+    $my_cnf = '';
 
     // comment the next two lines out if u want to allow passwords to be shown in 'ps' output (NOT RECOMMENDED)
     if (!empty($server['password']))
-        trigger_error('Cannot use specified server password - secure password functionality not implemented yet!', E_USER_ERROR);
+    {
+        $my_cnf = '/tmp/ms3b-my.cnf';
+
+        $t  = "[client]\n";
+        $t .= "password=$server[password]\n";
+
+        if (file_put_contents($my_cnf, $t, LOCK_EX) === false)
+        {
+            trigger_error("Couldn't write password to $my_cnf. Skipping backup of this server", E_USER_WARNING);
+            continue;
+        }
+
+    }
 
     // set the mysql args (common to both mysql (the client) and mysqldump)
-    $mysql_args = ($server['host'] ? "-h $server[host] " : '').
-        ($server['user']     ? "-u $server[user] "    : '').
-        ($server['password'] ? "-p$server[password] " : '');
+    $mysql_args = ($my_cnf ? "--defaults-file=$my_cnf " : '').
+        ($server['host'] ? "-h $server[host] " : '').
+        ($server['user'] ? "-u $server[user] " : '');
+        
 
     // Fetch list of databases from MySQL client
-    $cmd = "mysql $mysql_args --batch --skip-column-names -e 'SHOW DATABASES WHERE $server[db_where]'";
+    $cmd = "mysql $mysql_args --batch --skip-column-names -e 'SHOW DATABASES WHERE $server[db_where]' < /dev/null";
 
     // unset $databases otherwise exec() appends to the end of it
     unset($databases);
 
-    echo "Fetching DB list. Cmd to exec() : $cmd\n";
+    log_notice("Fetching DB list. Cmd to exec() : $cmd");
     $ret = 0;
     exec($cmd, $databases, $ret);
     if ($ret) trigger_error('exec() returned '.$ret, E_USER_ERROR);
@@ -125,11 +167,18 @@ foreach ($ms3b_cfg['Servers'] as $server)
 
     if ($server['exec_pre'])
     {
-        echo "Running: $server[exec_pre]\n";
+        log_notice("Running: $server[exec_pre]");
         $ret = 0;
         system($server['exec_pre'], $ret);
         if ($ret)
+        {
             trigger_error("Warning: exec_pre returned $ret.\n", E_USER_WARNING);
+
+            // uncomment this if u want exec_pre failure to stop exec_post from running
+            //$server['exec_post'] = '';
+        }
+        pcntl_signal_dispatch();
+        // should we continue; to next server here?  or should we ensure we don't run exec_post at end?
     }
 
     //----------------------------------------------------------------------------------------
@@ -141,14 +190,14 @@ foreach ($ms3b_cfg['Servers'] as $server)
         if (!$d) trigger_error('$d is false', E_USER_ERROR);
 
         // Do the backup
-        echo "Backing up database: $d\n";
+        log_notice("Backing up database: $d");
         $table_args = '';
 
         if (isset($server['tables_where'][$d]) && $server['tables_where'][$d])
         {
-            $cmd = "mysql $mysql_args $d --batch --skip-column-names -e 'SHOW TABLES WHERE $server[tables_where][$d]'";
+            $cmd = "mysql $mysql_args $d --batch --skip-column-names -e 'SHOW TABLES WHERE $server[tables_where][$d]' < /dev/null";
 
-            echo "Selective backup chosen. Getting list of tables to backup. Cmd to exec() : $cmd\n";
+            log_notice("Selective backup chosen. Getting list of tables to backup. Cmd to exec() : $cmd");
 
             // unset $tables otherwise exec() appends to the end of it
             unset($tables);
@@ -159,18 +208,18 @@ foreach ($ms3b_cfg['Servers'] as $server)
             foreach ($tables as $table)
             {
                 $table_args .= ' '.escapeshellarg($table);
-                echo "We will backup table: $table\n";
+                log_notice("We will backup table: $table");
             }
         }
         else
         {
-            echo "Backing up all tables.\n";
+            log_notice("Backing up all tables");
         }
 
-        //TIDY: .gpg is the convention (see http://lists.gnupg.org/pipermail/gnupg-users/2008-July/033898.html)
+        // .gpg is the convention (see http://lists.gnupg.org/pipermail/gnupg-users/2008-July/033898.html)
         $dest_file = "$this_backup_dir/$d.sql.gz.gpg";
 
-        error_log('['.date('Y-m-d H:i:s')."] Starting back up of database '$d' to $dest_file\n", 3, $ms3b_cfg['log']);
+        log_notice("Starting back up of database '$d' to $dest_file");
 
         // this bit means we have a DROP DATABASE and CREATE DATABASE line at the stop of each dump (unless we are dumping only certain tables)
         $other_args = '';
@@ -184,18 +233,25 @@ foreach ($ms3b_cfg['Servers'] as $server)
         $cmd = '(set -o pipefail && mysqldump '.$mysql_args.'--opt --quote-names '.$ms3b_cfg['mysqldump_args'].' '.$other_args.escapeshellarg($d).' '.$table_args.' | '.
                 $ms3b_cfg['compressor_cmd'].' | '.
                 'gpg -e '.($server['gpg_sign'] ? '-s ' : '').'-r '.$server['gpg_rcpt']." > $dest_file".
-                ')';
-        echo "Running: $cmd\n";
-
+                ') < /dev/null';
+        log_notice("Running: $cmd");
         
         $ret = 0;
         system($cmd, $ret);
-
-        error_log('['.date('Y-m-d H:i:s').'] Finished. Resulting file is '.filesize($dest_file)." bytes\n", 3, $ms3b_cfg['log']);
+        
 
         if ($ret)
-            trigger_error('system() call returned error code '.$ret.' for: '.$cmd, E_USER_WARNING);
-
+        {
+            if (unlink($dest_file))
+                trigger_error('system() call returned error code '.$ret.'. Backup file deleted. Command: '.$cmd, E_USER_WARNING);
+            else
+                trigger_error('system() call returned error code '.$ret.'. Failed to delete backup file. Command: '.$cmd, E_USER_WARNING);
+        }
+        else
+        {
+            log_notice('Pipe complete. Resulting file is '.filesize($dest_file)." bytes");
+        }
+        pcntl_signal_dispatch();
         /*
         // we used to echo ${PIPESTATUS[*]} and examine this but I think using pipefail is more elegant
         if (!preg_match('/^0( 0)*$/', $pipe_res))
@@ -206,56 +262,49 @@ foreach ($ms3b_cfg['Servers'] as $server)
 
     if ($server['exec_post'])
     {
-        echo "Running: $server[exec_post]\n";
+        log_notice("Running: $server[exec_post]");
         system($server['exec_post'], $ret);
         if ($ret)
             trigger_error("Warning: exec_post returned $ret\n", E_USER_WARNING);
+        pcntl_signal_dispatch();
     }
     $server['exec_post'] = false;
 
 
     // create a new bucket if necessary
     // TODO: test for presence of bucket and only issue this if necessary
-    $cmd = 's3cmd mb s3://'.$server['s3_bucket'];
-    echo "Running: $cmd\n";
+    $cmd = 's3cmd mb s3://'.$server['s3_bucket'].' < /dev/null';
+    log_notice("Running: $cmd");
     system($cmd, $ret);
-
+    pcntl_signal_dispatch();
 
     // Copy new backup dir to S3
-    echo "Uploading backup $now to S3 bucket $server[s3_bucket] ($server[s3_dir])...\n";
+    log_notice("Starting upload to Amazon S3 s3://$server[s3_bucket]$server[s3_dir]");
 
-    error_log('['.date('Y-m-d H:i:s')."] Starting upload to Amazon S3 s3://$server[s3_bucket]$server[s3_dir]\n", 3, $ms3b_cfg['log']);
-
-    $cmd = 'cd '.$ms3b_cfg['data_dir'].' && '.$ms3b_cfg['s3_cmd'] .' --no-encrypt '.$now.' s3://'.$server['s3_bucket'].$server['s3_dir'];
-    echo "Running: $cmd\n";
+    $cmd = 'cd '.$ms3b_cfg['data_dir'].' && '.$ms3b_cfg['s3_cmd'] .' --no-encrypt '.$now.' s3://'.$server['s3_bucket'].$server['s3_dir'].' < /dev/null';
+    log_notice("Running: $cmd");
     system($cmd, $ret);
-
-    error_log('['.date('Y-m-d H:i:s')."] S3 Upload complete\n", 3, $ms3b_cfg['log']);
-    echo "S3 copy done.\n";
     
     if ($ret)
     {
-        trigger_error('s3cmd returned '.$ret, E_USER_WARNING);
+        //TODO: i don't think we are ever reaching this part - I think s3cmd might never return a non-zero error code
+        trigger_error('s3cmd returned '.$ret.' - skipping delete of local files', E_USER_WARNING);
+        pcntl_signal_dispatch();
         continue; //foreach (skip local delete)
     }
+    pcntl_signal_dispatch();
 
-    // Remove local copy of backups
-    // disabled until we can work out a way of running s3cmd and get a non-zero error code when some files don't upload
-
-    // TODO: uncomment this or we are going to run out of disk space!!!!
-
-    /*
-    echo "Removing backup dir ($this_backup_dir)\n";
+    log_notice("S3 Upload complete. Removing backup dir ($this_backup_dir)");
 
     // sanity check
-    if (realpath($this_backup_dir) == '/') trigger_error('Refusing to wipe entire filesystem!', E_USER_ERROR);
+    if (realpath($this_backup_dir) == '/') 
+        trigger_error('Refusing to wipe entire filesystem!', E_USER_ERROR);
 
     system('rm -rf '.$this_backup_dir, $ret);
-    if ($ret) trigger_error('system() call returned '.$ret, E_USER_WARNING);
-    */
+    if ($ret) trigger_error('rm system() call returned '.$ret, E_USER_WARNING);
 
-    echo "All done.\n";
+    log_notice("Finished with backup of (host:'$server[host]', user:'$server[user]')...");
 }
 
+log_notice('mysql_s3_backup ended normally');
 $clean_shutdown = true;
-error_log('['.date('Y-m-d H:i:s')."] * mysql_s3_backup ended normally\n", 3, $ms3b_cfg['log']);
